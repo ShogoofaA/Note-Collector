4a7b8bfb58c7da5592e24267801b4cd156f1059e
Shogoofa Akbari <shogoofa.musket@gmail.com>
2022-06-13 13:35:19 -0700

    added functions to save new notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,15 @@
-const path = require('path');
-const express = require('express');
-const router = express.Router();
-  
-  router.get('/api/notes', (req, res) => {
-    console.log('hello')
-    res.json({ok: true})
-  });
+const router = require('express').Router();
+const {notes} = require('../../db/db');
+const {createNote} = require('../../lib/notes');
+
+router.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNote(req.body, notes);
+    res.json(note);
+});
 
 module.exports = router;
